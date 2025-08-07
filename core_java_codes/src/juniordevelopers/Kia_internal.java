6c8f2b3ea0542bce2fc68764be0865249c33d7b7
harshitha <harshithakv.22ds@saividya.ac.in>
2025-08-07 14:41:03 +0530

    my 3rd commit

---
+++ core_java_codes/src/juniordevelopers/Kia_internal.java
@@ -0,0 +1,12 @@
+package juniordevelopers;
+
+public class Kia_internal {
+	public static void main(String[] args) {
+		juniordevelopers.Kia_Airbags obj=new juniordevelopers.Kia_Airbags();
+		obj.disp_airbags();
+		juniordevelopers.Kia_speedlimit obj1=new juniordevelopers.Kia_speedlimit();
+		obj1.disp_speedlimit();
+	}
+
+
+}

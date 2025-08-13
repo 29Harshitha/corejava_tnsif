12a18b501d4dee47c5846ff50f8683ad03811e9d
harshitha <harshithakv.22ds@saividya.ac.in>
2025-08-13 19:21:17 +0530

    my 5th commit

---
+++ core_java_codes/src/oops/singleinheritence.java
@@ -0,0 +1,30 @@
+package oops;
+class Course //parent
+{
+	String coursename="Java Programming";
+	
+	void showCourse()
+	{
+		System.out.println("Course :"+coursename);
+	}
+}
+class Student extends Course //child
+{
+	String name="Amit";
+	
+	void showStudent()
+	{
+		System.out.println("Student:" + name);
+	}
+}
+
+public class singleinheritence {
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.showStudent();
+		s.showCourse();
+
+	}
+
+
+}
